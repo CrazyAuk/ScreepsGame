cb8194a135141cfc045e33597b6694d32f02d701
Adrien Makosso <titoumak78@hotmail.fr>
2021-03-01 01:18:34 +0100

    Manager Role et Spawn fini

--- AutoSpawn.js
+++ AutoSpawn.js
@@ -5,119 +5,92 @@
  * 
  */
 
-var AutoSpawn = {
+var spawnManager = {
 
     run: function(tier) {
-     
-    
-
-    }
-}
-
-var upgrader = {
-
-    run: function(spawner,tier) {
-    
-        var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-        console.log('Upgraders: ' + upgrader.length);
-    
-        var canSpawn = Game.spawns[spawner].spawnCreep(tier, 'test', { dryRun: true });
         
-        if (canSpawn == 0) {
-            for(var name in Memory.creeps) {
-                if(!Game.creeps[name]) {
-                    delete Memory.creeps[name];
-                    console.log('Clearing non-existing creep memory:', name);
-                }
-            }
-            
-            if(upgrader.length < 1) {
-                var newName = 'Upgrader' + Game.time;
-                console.log('Spawning new upgrader: ' + newName);
-                Game.spawns[spawner].spawnCreep(tier, newName, 
-                    {memory: {role: 'upgrader'}});
-            }
-            
-            if(Game.spawns[spawner].spawning) { 
-                var spawningCreep = Game.creeps[spawner.spawning.name];
-                sapwner.room.visual.text(
-                    '🛠️' + spawningCreep.memory.role,
-                    Game.spawns[spawner].pos.x + 1, 
-                    Game.spawns[spawner].pos.y, 
-                    {align: 'left', opacity: 0.8});
+        for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
             }
         }
+        
+        for(var name in Game.spawns){
+            let canSpawn = Game.spawns[name].spawnCreep(tier, 'test', { dryRun: true });
+            if (canSpawn === 0) {
+                var spawner = Game.spawns[name];
+                break;
+            }
+        }
+        
+        let harvester = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        let builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        let upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        
+        if(harvester.length < 3) {
+            harvesterSpawn.run(spawner,tier);
+        }else{
+            if(builder.length < 2) {
+                builderSpawn.run(spawner,tier);
+            }else{
+                if(upgrader.length < 1) {
+                    upfraderSpawn.run(spawner,tier);
+                }
+            }
+        }
+        
+        if(Game.spawns[spawner].spawning) { 
+            var spawningCreep = Game.creeps[Game.spawns[spawner].spawning.name];
+            spawner.room.visual.text(
+                '🛠️' + spawningCreep.memory.role,
+                Game.spawns[spawner].pos.x + 1, 
+                Game.spawns[spawner].pos.y, 
+                {align: 'left', opacity: 0.8});
+        }
+     
     }
 };
 
-var builderAutoSpawn = {
+
+var upgraderSpawn = {
 
     run: function(spawner,tier) {
     
-        var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-        console.log('Builders: ' + builder.length);
-    
-        var canSpawn = Game.spawns[spawner].spawnCreep(tier, 'test', { dryRun: true });
-        
-        if (canSpawn == 0) {
-            for(var name in Memory.creeps) {
-                if(!Game.creeps[name]) {
-                    delete Memory.creeps[name];
-                    console.log('Clearing non-existing creep memory:', name);
-                }
-            }
+        //var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        //console.log('Upgraders: ' + upgrader.length);
             
-            if(builder.length < 2) {
-                var newName = 'Builder' + Game.time;
-                console.log('Spawning new builder: ' + newName);
-                Game.spawns[spawner].spawnCreep(tier, newName, 
-                    {memory: {role: 'builder'}});
-            }
-            
-            if(Game.spawns[spawner].spawning) { 
-                var spawningCreep = Game.creeps[spawner.spawning.name];
-                Game.spawns[spawner].room.visual.text(
-                    '🛠️' + spawningCreep.memory.role,
-                    Game.spawns[spawner].pos.x + 1, 
-                    Game.spawns[spawner].pos.y, 
-                    {align: 'left', opacity: 0.8});
-            }
-        }
+        var newName = 'Upgrader' + Game.time;
+        console.log('Spawning new upgrader: ' + newName);
+        Game.spawns[spawner].spawnCreep(tier, newName, {memory: {role: 'upgrader'}});
     }
-}
+};
 
-var harvesterAutoSpawn = {
+var builderSpawn = {
 
     run: function(spawner,tier) {
     
-        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        console.log('Harvesters: ' + harvesters.length);
+        //var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        //console.log('Builders: ' + builder.length);
     
-        var canSpawn = Game.spawns[spawner].spawnCreep(tier, 'test', { dryRun: true });
-        
-        if (canSpawn == 0) {
-            for(var name in Memory.creeps) {
-                if(!Game.creeps[name]) {
-                    delete Memory.creeps[name];
-                    console.log('Clearing non-existing creep memory:', name);
-                }
-            }
-            
-            if(harvesters.length < 3) {
-                var newName = 'Harvester' + Game.time;
-                console.log('Spawning new harvester: ' + newName);
-                Game.spawns[spawner].spawnCreep(tier, newName, 
-                    {memory: {role: 'harvester'}});
-            }
-            
-            if(Game.spawns[spawner].spawning) { 
-                var spawningCreep = Game.creeps[Game.spawns[spawner].spawning.name];
-                sapwner.room.visual.text(
-                    '🛠️' + spawningCreep.memory.role,
-                    Game.spawns[spawner].pos.x + 1, 
-                    Game.spawns[spawner].pos.y, 
-                    {align: 'left', opacity: 0.8});
-            }
-        }
+        var newName = 'Builder' + Game.time;
+        console.log('Spawning new builder: ' + newName);
+        Game.spawns[spawner].spawnCreep(tier, newName, {memory: {role: 'builder'}});
     }
-}
+};
+
+var harvesterSpawn = {
+
+    run: function(spawner,tier) {
+    
+        //var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        //console.log('Harvesters: ' + harvesters.length);
+    
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns[spawner].spawnCreep(tier, newName, {memory: {role: 'harvester'}});
+        
+    }
+};
+
+module.exports = spawnManager;
--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var harvesterAutoSpawn = require('harvester.autoSpawn');
-var builderAutoSpawn = require('builder.autoSpawn');
-var upgraderAutoSpawn = require('upgrader.autoSpawn');
+var roleHarvester = require('roleManager');
+var roleUpgrader = require('roleManager');
+var roleBuilder = require('roleManager');
+var spawnManager = require('autoSpawn');
+var harvesterAutoSpawn = require('autoSpawn');
+var builderAutoSpawn = require('autoSpawn');
+var upgraderAutoSpawn = require('autoSpawn');
  
 const T1 = [WORK,CARRY,MOVE];
 const T2 = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
 
 module.exports.loop = function () {
     
-    for(var name in Game.spawns) {
-        harvesterAutoSpawn.run(name,T1);
-        builderAutoSpawn.run(name,T1);
-        upgraderAutoSpawn.run(name,T1);
-    }
+    spawnManager.run(T1);
     
 	for(var name in Game.creeps) {
         var creep = Game.creeps[name];
